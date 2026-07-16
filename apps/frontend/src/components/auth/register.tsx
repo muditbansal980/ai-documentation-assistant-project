092a1ad0789ab23d3ebed3d22bad6068d91f6932
muditbansal980 <muditb512@gmail.com>
2026-07-16 21:56:33 +0530

    register frontend

--- apps/frontend/src/components/auth/register.tsx
+++ apps/frontend/src/components/auth/register.tsx
@@ -2,6 +2,16 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { RegisterUserInput } from "@/types/auth/authtypes";
+import {RegisterUser} from "@/api/auth/auth"
+
+
+
+function handleRegister(data:RegisterUserInput){
+    RegisterUser(data).then((result)=>{
+        console.log(result)
+    })
+}
 
 export default function Register(){
     const router = useRouter();
@@ -30,6 +40,15 @@ export default function Register(){
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
+            <button onClick={()=>{
+                handleRegister({
+                    Username: username,
+                    Email: email,
+                    Password: password
+                })
+            }}>
+                Register
+            </button>
             <div>
                 <button onClick={() => router.push("/login")}>Already have an account</button>
             </div>
