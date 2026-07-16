a02b4991b16cdc3a5aa2db2239cdacf7a1d27308
muditbansal980 <muditb512@gmail.com>
2026-07-16 22:01:08 +0530

    added the graphql schema with the cors

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
-from app.api.routes.routes import router
+from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.graphql.schema import schema
+
 app = FastAPI()
+
+graphql_app = GraphQLRouter(schema)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True, 
+)
+
+
 app.include_router(
-    router
+    graphql_app,
+    prefix="/graphql"
 )
