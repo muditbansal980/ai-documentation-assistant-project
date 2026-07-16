3c3302f1a8ef1b41e0969a8a3ee8e805aa867076
muditbansal980 <muditb512@gmail.com>
2026-07-16 22:10:39 +0530

    models for user

--- apps/backend/app/models/users/user.py
+++ apps/backend/app/models/users/user.py
@@ -9,7 +9,7 @@ class User(Base):
     Id:Mapped[str] = mapped_column(String,primary_key=True, default=uuid.uuid4)
     Username:Mapped[str] = mapped_column(String(50),nullable=False,unique=True)
     Email:Mapped[str] = mapped_column(String(100),nullable=False,unique=True)
-    Password:Mapped[str] = mapped_column(String(100),nullable=False)
+    Password:Mapped[str] = mapped_column(String(100),nullable=True)
 class UserLogin(Base):
     __tablename__ = "User"
     
