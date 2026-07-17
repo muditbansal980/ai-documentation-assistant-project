a666a883b5ac9a1dd803d19e5a6435e62b1f5177
muditbansal980 <muditb512@gmail.com>
2026-07-17 22:09:41 +0530

    multipart upload enabled true in graphqlrouter

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.core.config import UPLOAD_DIR
 from app.graphql.schema import schema
 
 app = FastAPI()
 
-graphql_app = GraphQLRouter(schema)
+graphql_app = GraphQLRouter(
+    schema,
+    multipart_uploads_enabled=True,
+)
 
 
 app.add_middleware(
