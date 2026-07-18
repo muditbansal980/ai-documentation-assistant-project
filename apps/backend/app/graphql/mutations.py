ff6c00b050211784eae16b0135203498b05ce511
muditbansal980 <muditb512@gmail.com>
2026-07-18 13:25:13 +0530

    proper error handling for file uplaoding

--- apps/backend/app/graphql/mutations.py
+++ apps/backend/app/graphql/mutations.py
@@ -7,6 +7,9 @@ from app.schemas.auth.register import UserRegisterSchema,UserLoginSchema
 from strawberry.file_uploads import Upload
 from app.graphql.types import MessageResponse
 from app.api.controllers.file.upload_file import UploadFile
+from app.utils.auth.auth_utils import get_current_user
+from app.graphql.types import AuthError
+from typing import Union, Annotated
 @strawberry.type
 class Mutation:
     @strawberry.mutation 
@@ -46,12 +49,19 @@ class Mutation:
         )
         
     # upload file mutation
+
+    UploadFileResult = Annotated[
+        Union[MessageResponse, AuthError],
+        strawberry.union("UploadFileResult"),
+    ]
     @strawberry.mutation
-    async def UploadFile(self,info:strawberry.Info,file:Upload) ->MessageResponse:
-        user = info.context["user"]
+    async def UploadFile(self,info:strawberry.Info,file:Upload) ->UploadFileResult:
+        user = get_current_user(info)
         if not user:
-            raise Exception("Not authenticated")
+            return AuthError()  # uses default "Not authenticated" message
         controller = await UploadFile(file,user)
+        if controller.error:
+           return AuthError(message=controller.error)
         return MessageResponse(
             message="File uploaded successfully."
         )
