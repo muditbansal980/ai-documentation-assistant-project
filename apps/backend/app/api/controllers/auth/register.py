9f20fcbcd176f874daccfe73874f4f93c9ae0c22
muditbansal980 <muditb512@gmail.com>
2026-07-18 12:09:52 +0530

    creating of jwt token and password hashing in backend

--- apps/backend/app/api/controllers/auth/register.py
+++ apps/backend/app/api/controllers/auth/register.py
@@ -5,6 +5,7 @@ from app.models.users.user import User
 from app.schemas.auth.login import UserLoginSchema
 from app.schemas.auth.register import UserRegisterSchema
 from pwdlib import PasswordHash
+from app.services.auth.auth import create_access_token
 
 async def register_user(register_data: UserRegisterSchema):
     try: 
@@ -35,11 +36,17 @@ async def login_user(login_data: UserLoginSchema):
             )
         )
             user = query.scalar_one_or_none()
+            
+            print("User found:", user)
             if(user is None):
                 return {"message": "No such user found"}
             else:
+                # Create a JWT token for the user
+                token = create_access_token(
+                    data={"sub": str(user.Id), "username": user.Username, "email": user.Email}
+                )
                 if(password_hash.verify(login_data.Password, user.Password)):
-                    return {"message": "Login successful"}
+                    return {"message": "Login successful", "auth_token": token}
                 else:
                     return {"message": "Incorrect password"}
     except Exception as e:
