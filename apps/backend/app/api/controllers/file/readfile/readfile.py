1a69a73c59a20b574c4d5bc747470281f39ddbd0
muditbansal980 <muditb512@gmail.com>
2026-07-18 20:17:07 +0530

    changed format of chunk storage

--- apps/backend/app/api/controllers/file/readfile/readfile.py
+++ apps/backend/app/api/controllers/file/readfile/readfile.py
@@ -1,15 +1,33 @@
 import pymupdf 
-async def extract_pdf_text(path: str):
+from app.api.controllers.file.chunking.chunking_file import split_text_into_chunks
+from app.api.controllers.file.chunking.chunking_file import save_chunks_to_database
+async def extract_pdf_text(path: str, document_id: str):
 
     document = pymupdf.open(path)
     text = ""
     pages = []
+    all_chunks = []
     for page_number, page in enumerate(document):
     
         pages.append(
             {
                 "page": page_number + 1,
-                "text": page.get_text()
+                "text": page.get_text(),
             }
         )
-    return pages
+        chunks = split_text_into_chunks(
+        page.get_text()
+       )
+        for index, chunk in enumerate(
+        chunks,
+        start=1
+        ):
+            all_chunks.append(
+                {
+                    "page": page_number + 1,
+                    "chunk_number": index,
+                    "text": chunk
+                }
+            )
+    await save_chunks_to_database(document_id=document_id, chunks=all_chunks)
+    return {"pages": pages, "chunks": all_chunks}
