f93a5acf2ea5605a49af1345073a71b18e556c27
muditbansal980 <muditb512@gmail.com>
2026-07-18 13:25:54 +0530

    proper error handling for uplaoding file as it is a protected route

--- apps/backend/app/graphql/context.py
+++ apps/backend/app/graphql/context.py
@@ -6,8 +6,11 @@ async def get_context(request: Request):
     auth_header = request.headers.get("Authorization")
     if auth_header and auth_header.startswith("Bearer "):
         token = auth_header.removeprefix("Bearer ")
+        print(f"Token received: {token}")  # Debugging line to check the token value
         payload = decode_access_token(token)
         if payload:
             user = payload  # dict with sub/username/email
-
+        if(not token):
+            print("No token provided in the Authorization header.")
+            return {"request": request, "user": None}
     return {"request": request, "user": user}
