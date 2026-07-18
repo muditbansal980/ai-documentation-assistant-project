2e85bd144fb210d04524387005cf4f199b883105
muditbansal980 <muditb512@gmail.com>
2026-07-18 13:58:10 +0530

    reading file text

--- apps/backend/app/api/controllers/file/readfile/readfile.py
+++ apps/backend/app/api/controllers/file/readfile/readfile.py
@@ -1,1 +1,15 @@
 import pymupdf 
+async def extract_pdf_text(path: str):
+
+    document = pymupdf.open(path)
+    text = ""
+    pages = []
+    for page_number, page in enumerate(document):
+    
+        pages.append(
+            {
+                "page": page_number + 1,
+                "text": page.get_text()
+            }
+        )
+    return pages
