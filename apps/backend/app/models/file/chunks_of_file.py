3bc1eaaa9d972504beee85e519acb4e2806aa4db
muditbansal980 <muditb512@gmail.com>
2026-07-18 20:18:56 +0530

    model for storing chunks

---
+++ apps/backend/app/models/file/chunks_of_file.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Integer, String
+from app.db.base import Base
+import uuid
+
+class DocumentChunks(Base):
+    __tablename__ = "DocumentChunks"
+
+    Id: Mapped[str] = mapped_column(String, primary_key=True, default=uuid.uuid4)
+    DocumentId: Mapped[str] = mapped_column(String(50), nullable=False)
+    PageNumber: Mapped[int] = mapped_column(Integer, nullable=False)
+    ChunkNumber: Mapped[int] = mapped_column(Integer, nullable=False)
+    ChunkText: Mapped[str] = mapped_column(String, nullable=False)
