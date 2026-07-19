dc4587c7cce4a91bde65d2a5396ca1f6724ae396
muditbansal980 <muditb512@gmail.com>
2026-07-19 23:41:10 +0530

    added the userid in the model documentschunks

--- apps/backend/app/models/file/chunks_of_file.py
+++ apps/backend/app/models/file/chunks_of_file.py
@@ -9,7 +9,8 @@ class DocumentChunks(Base):
     __tablename__ = "DocumentChunks"
 
     Id: Mapped[str] = mapped_column(String, primary_key=True, default=uuid.uuid4)
-    DocumentId: Mapped[str] = mapped_column(String(50), nullable=False)
+    DocumentId: Mapped[str] = mapped_column(String(50), nullable=True)
+    UserId: Mapped[str] = mapped_column(String(50), nullable=False)
     PageNumber: Mapped[int] = mapped_column(Integer, nullable=False)
     ChunkNumber: Mapped[int] = mapped_column(Integer, nullable=False)
     ChunkText: Mapped[str] = mapped_column(String, nullable=False)
