cd04f26f9a45092cc0905dae45d1a318000cc0ff
muditbansal980 <muditb512@gmail.com>
2026-07-19 22:14:32 +0530

    routing to chat path after successfully uploading the file

--- apps/frontend/src/components/home/uploadfile/upload.tsx
+++ apps/frontend/src/components/home/uploadfile/upload.tsx
@@ -1,9 +1,13 @@
 "use client"
-import { useRef} from "react";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation"
 import { UploadFile } from "@/api/file_upload/file_upload";
 
 export default function UploadFiles() {
+
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const [documentId, setDocumentId] = useState<string | null>(null);
+    const router = useRouter();
     const uploadFile = async (
         e: React.ChangeEvent<HTMLInputElement>
     ) => {
@@ -11,7 +15,7 @@ export default function UploadFiles() {
             console.log("No file selected");
             return;
         }
-        if(e.target.files[0].type !== "application/pdf") {
+        if (e.target.files[0].type !== "application/pdf") {
             alert("Invalid file type. Please select a PDF file.");
             return;
         }
@@ -24,11 +28,23 @@ export default function UploadFiles() {
             console.log("Uploading file: " + file.name);
             const result = await UploadFile(file);
             console.log(result);
+            if (result.data.UploadFile.__typename === "SuccessfulFileUploadMessage") {
+                setDocumentId(result.data.UploadFile.documentId);
+                console.log("File uploaded successfully. Document ID: " + result.data.UploadFile.documentId);
+
+            } else {
+                console.log("Error uploading file: " + result.data.UploadFile.message);
+            }
         } catch (error) {
             console.log(error);
         }
     };
-
+    useEffect(()=>{
+        if(documentId){
+            router.push(`/chat/${documentId}`);
+        }
+        return;
+    },[documentId, router])
     return (
         <div>
             <input accept=".pdf,application/pdf" type="file" ref={fileInputRef} onChange={uploadFile} />
