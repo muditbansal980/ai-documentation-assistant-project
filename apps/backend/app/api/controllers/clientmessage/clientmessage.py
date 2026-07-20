4421588ef43dcc1fda92ec0a2d64dbc10511caef
muditbansal980 <muditb512@gmail.com>
2026-07-20 16:08:50 +0530

    connected llm to the client message function

--- apps/backend/app/api/controllers/clientmessage/clientmessage.py
+++ apps/backend/app/api/controllers/clientmessage/clientmessage.py
@@ -2,6 +2,7 @@ from .embeddings_generation.emb_gen_client import gen_emb_client
 from app.utils.auth.auth_utils import get_current_user
 from app.models.client_message.client_message import ClientMessages
 from app.db.session import AsyncSessionLocal
+from app.api.controllers.llm.llm import generate_response
 import uuid
 import strawberry
 from app.services.client_file_embeddings_matching.matching import ClientMessage_File_Matching
@@ -33,7 +34,11 @@ async def ClientMessage(message:str,docId:str,info:strawberry.Info):
         
         # Call the ClientMessage_File_Matching function to match the client message with document chunks
         matching_result = await ClientMessage_File_Matching(docId=DocumentId, info=info, clientMsgEmb=embeddings)
-        print("Matching result:", matching_result)
-        return {"message": "Client message successfully embedded and inserted in db"}
+        print("<-------------------Matching result-------------------->\n\n\n\n")
+        print("Matching result(context):", matching_result)
+        print("<-------------------Matching result-------------------->\n\n\n\n")
+        context = [item[0] for item in matching_result[1]]
+        providing_context = await generate_response(client_message=ClientMess, context=context)
+        return {"message": "Client message successfully embedded and inserted in db and response generated"}
     except Exception as e:
         print("Error occured:-",e)
