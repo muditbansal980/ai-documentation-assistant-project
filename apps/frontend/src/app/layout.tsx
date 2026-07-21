129bb33e04e1c4ffb6f36a7b0bcd494981a8dc2d
muditbansal980 <muditb512@gmail.com>
2026-07-21 16:36:14 +0530

    added tailwind.confg.ts file for ui

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,4 +1,8 @@
-
+import "./styles/globals.css"
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "My App",
+};
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html>
